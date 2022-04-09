52ce4e852ae5777b99d5b9024c0e30794c84d51f
tbeachill <82418020+tbeachill@users.noreply.github.com>
2022-04-09 22:45:49 +0100

    finished exercise

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -4,9 +4,10 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        client::connect();
     }
 }
